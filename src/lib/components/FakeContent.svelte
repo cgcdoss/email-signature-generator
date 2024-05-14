a34f79742ffb2181fc94a2be8e85b254b541b10e
cgcdoss <gabi04061997@gmail.com>
2024-05-13 22:31:38 -0300

    feat: melhorando organização da tela

---
+++ src/lib/components/FakeContent.svelte
@@ -0,0 +1,5 @@
+<div>
+    <div class="mt-4 p-2 bg-gray-300 w-48"></div>
+    <div class="mt-1 p-2 bg-gray-300 w-36"></div>
+    <div class="mt-1 p-2 bg-gray-300 w-28"></div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import FakeContent from "$lib/components/FakeContent.svelte";
     import { getLowerQualityImg } from "$lib/utils";
 
     let img = "";
@@ -35,6 +36,18 @@
         }
     }
 
+    function handleOnPast(event: ClipboardEvent) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const img = new Image();
+            img.onload = () => {
+                imgFile = getLowerQualityImg(img);
+            };
+            img.src = e.target!.result as string;
+        };
+        reader.readAsDataURL(event.clipboardData?.files[0]!);
+    }
+
     function cleanImgFile() {
         const input = document.querySelector(
             'input[type="file"]'
@@ -48,9 +61,15 @@
     <title>Gerar assintatura de e-mail (sem cadastro)</title>
 </svelte:head>
 
-<main class="flex flex-col sm:grid grid-cols-1 sm:grid-cols-10 min-h-[100vh]">
+<header class="p-4 bg-gray-600">
+    <h1 class="text-2xl text-white">Gerar Assinatura de E-mail</h1>
+</header>
+
+<main
+    class="flex flex-col sm:grid grid-cols-1 sm:grid-cols-10 min-h-[calc(100vh-64px)]"
+>
     <section
-        class="form flex flex-col gap-2 col-span-3 px-4 py-8 sm:min-h-[100vh] shadow-md"
+        class="form flex flex-col gap-2 col-span-3 px-4 py-8 sm:h-full shadow-md"
     >
         <h2 class="text-xl">
             Preencha as informações abaixo e depois clique em Copiar assinatura
@@ -60,10 +79,17 @@
             e-mail
         </p>
 
+        <div class="p-2 px-4 bg-gray-200 -mx-4 mb-2">Geral</div>
+
+        <input type="text" placeholder="Nome" bind:value={name} />
+        <input type="text" placeholder="Cargo" bind:value={cargo} />
+        <input type="text" placeholder="Empresa" bind:value={empresa} />
+        <input type="text" placeholder="Telefone" bind:value={telefone} />
         <div class="flex max-sm:flex-col sm:items-center max-sm:mb-3 gap-2">
             <input
                 type="text"
-                placeholder="URL da imagem"
+                placeholder="URL da imagem/Ctrl + V"
+                on:paste={handleOnPast}
                 bind:value={img}
                 disabled={!!imgFile}
             />
@@ -80,10 +106,6 @@
                 </button>
             {/if}
         </div>
-        <input type="text" placeholder="Nome" bind:value={name} />
-        <input type="text" placeholder="Cargo" bind:value={cargo} />
-        <input type="text" placeholder="Empresa" bind:value={empresa} />
-        <input type="text" placeholder="Telefone" bind:value={telefone} />
 
         {#if name}
             <button
@@ -96,14 +118,18 @@
     </section>
 
     <section
-        class="result flex flex-1 col-span-7 sm:min-h-[100vh] bg-gray-300 items-center justify-center py-8"
+        class="result flex flex-1 col-span-7 sm:h-full bg-gray-300 items-center justify-center py-8"
     >
         <div
             class="fake-email w-11/12 sm:w-1/2 sm:h-1/2 bg-white shadow-md p-2"
         >
-            <h2 class="bg-blue-200 pl-2">Novo email</h2>
-            <p class="border-b my-2">De</p>
-            <p class="border-b">Para</p>
+            <h2 class="bg-gray-200 text-gray-500 pl-2 p-3 -m-2 mb-3 font-bold">
+                Pré-visualização da assinatura
+            </h2>
+            <p class="border-b my-2 text-gray-400">Para</p>
+            <p class="border-b text-gray-400">Assunto</p>
+
+            <FakeContent />
 
             <p class="my-4">Atenciosamente,</p>
 
@@ -111,8 +137,8 @@
                 <table>
                     <tbody>
                         <tr>
-                            <td>
-                                {#if img || imgFile}
+                            {#if img || imgFile}
+                                <td>
                                     <div>
                                         <img
                                             src={img || imgFile}
@@ -121,9 +147,12 @@
                                             height="150"
                                         />
                                     </div>
-                                {/if}
-                            </td>
-                            <td class="align-top pl-2">
+                                </td>
+                            {/if}
+                            <td
+                                class="align-top pl-2"
+                                class:pl-2={img || imgFile}
+                            >
                                 <h2 class="text-xl font-bold">{name}</h2>
                                 {#if cargo || (cargo && empresa)}
                                     <h3 class="text-lg">
